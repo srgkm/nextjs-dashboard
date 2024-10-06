8fa806802d927f1bbf0952b34c5696779cdeeb4a
Sergei Kim <srg.kim@gmail.com>
2024-10-06 16:24:11 +0300

    ch11 finished

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -12,6 +12,7 @@ export default async function InvoicesTable({
   currentPage: number;
 }) {
   const invoices = await fetchFilteredInvoices(query, currentPage);
+  await new Promise((resolve) => setTimeout(resolve, 888));
 
   return (
     <div className="mt-6 flow-root">
